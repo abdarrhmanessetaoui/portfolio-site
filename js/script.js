7809d1e2f223e97a69f9a6ccf1b1f0e9370f6a56
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 13:26:34 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // ===== Menu toggle =====
   const menuIcon = document.getElementById('menu-icon');
   const navbar = document.querySelector('.navbar');
+  const dropdown = document.getElementById('langDropdown');
   menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
