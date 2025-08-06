b2f663d6cf9983090965f3988bc2d53a20c8bb8d
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 23:29:52 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -156,7 +156,7 @@ document.addEventListener('DOMContentLoaded', () => {
       "cybersecurity": "Notions de Cybersécurité",
       "cybersecurityDesc": "J’ai des connaissances fondamentales en cybersécurité pour aider à protéger les sites et les applications contre les menaces courantes, assurant la sécurité des données utilisateurs.",
       "network": "Bases des Réseaux",
-      "networkDesc": "Je comprends les concepts fondamentaux des réseaux, ce qui m’aide à optimiser les performances web et à résoudre efficacement les problèmes de connectivité."
+      "networkDesc": "Je comprends les concepts fondamentaux des réseaux, ce qui m’aide à optimiser les performances web et à résoudre efficacement les problèmes de connectivité.",
     
        },
     ar: {
