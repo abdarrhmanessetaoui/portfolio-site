f8f619f4ab289a93503a5555b4bfe3188940058d
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 14:51:34 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -127,9 +127,6 @@ flagIcons.forEach(icon => {
   if (lang === 'ar') {
     icon.style.marginLeft = '8px';  
     icon.style.marginRight = '0';
-  } else {
-    icon.style.marginRight = '8px';
-    icon.style.marginLeft = '0';
   }
 });
 
