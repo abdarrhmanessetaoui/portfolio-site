c076a6d5ae9601580c3392e44c4bfc57c8420230
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 14:46:06 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@ const dropdown = document.getElementById('langDropdown');
 if (dropdown) {
   if (lang === 'ar') {
     dropdown.style.textAlign = 'right';
-    dropdown.style.paddingRight = '1rem';
+    dropdown.style.paddingRight = '2rem';
     dropdown.style.paddingLeft = '0';
   } else {
     dropdown.style.textAlign = 'left';
