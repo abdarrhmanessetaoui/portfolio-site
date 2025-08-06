4982357ba0167ddbec0df26bb6e07e6513b4629c
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 15:32:44 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
       independentDesc: "Création et mise à jour de pages web en HTML/CSS, développement de solutions personnalisées pour des clients locaux et internationaux.",
       mobileTech: "Technicien Mobile",
       samir: "Samir Informatique",
-      samirDesc: "Réparation d'ordinateurs, maintenance système et formatage ; développement des compétences techniques en matériel et logiciel."
+      samirDesc: "Réparation d'ordinateurs, maintenance système et formatage ; développement des compétences techniques en matériel et logiciel."
        },
     ar: {
       //traduction for header
