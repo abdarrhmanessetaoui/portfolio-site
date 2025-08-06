caeb9d414c0ae9b7fef018171f36ff794d199296
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 13:43:38 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function isVisible(element) {
     const rect = element.getBoundingClientRect();
-    return rect.top <= window.innerHeight * 0.88885 && rect.bottom >= 0;
+    return rect.top <= window.innerHeight * 0.8888885 && rect.bottom >= 0;
   }
 
   function animateOnScroll() {
