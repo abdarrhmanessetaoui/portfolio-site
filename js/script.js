22b97801a33baacbf12f3a28568f27cf3f8b16f4
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 21:55:29 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -261,7 +261,7 @@ document.addEventListener('DOMContentLoaded', () => {
     elements2.forEach(el => {
       if (lang === 'ar') {
         el.style.marginRight = '36px';
-        el.style.marginLeft = '0';
+        el.style.marginLeft = '5px';
       } else {
         el.style.marginLeft = '6px';
         el.style.marginRight = '0';
