864eb043ce96739caa3f6792287c740d63048750
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-06 13:25:16 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-    dropdown.style('display:none;');
+    dropdown.css('display:none;');
   });
 
   // ===== Language Dropdown =====
