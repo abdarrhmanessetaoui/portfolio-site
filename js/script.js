880cb7d249b0b2f76d6a3bfc84c916a7ca6ac749
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-07 01:09:40 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -333,12 +333,12 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Translate input `value` attributes (like buttons)
-    document.querySelectorAll('[data-i18n-value]').forEach(el => {
-    const key = el.getAttribute('data-i18n-value');
-    if (translations[lang] && translations[lang][key]) {
+document.querySelectorAll('input[data-i18n-value]').forEach(el => {
+  const key = el.getAttribute('data-i18n-value');
+  if (translations[lang] && translations[lang][key]) {
     el.setAttribute('value', translations[lang][key]);
-   }
-  });
+  }
+});
 
     const elements = document.querySelectorAll('[data-i18n]');
     elements.forEach(el => {
