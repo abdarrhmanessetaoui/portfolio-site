2f14a01c4cbcc0ff98914c656bf1a0a6b7ff2635
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-07 01:00:31 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -324,10 +324,10 @@ document.addEventListener('DOMContentLoaded', () => {
   
 
   function applyTranslation(lang) {
-    // Translate placeholder attributes
+   // Translate placeholder attributes
    document.querySelectorAll('[data-i18n-placeholder]').forEach(el => {
    const key = el.getAttribute('data-i18n-placeholder');
-  if (translations[lang] && translations[lang][key]) {
+   if (translations[lang] && translations[lang][key]) {
     el.setAttribute('placeholder', translations[lang][key]);
   }
 });
