2d2a99cf733dcf2c6981213fd43c52c5b30b35e6
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-07 01:53:55 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var currentLang = document.documentElement.lang || "en";
   var logoImg = document.querySelector('.logo img');
   if (currentLang === "ar") {
-   logoImg.src = "img/lo";
+   logoImg.src = "img/logoarb";
     }
   // ===== Body visible on load =====
   document.body.classList.add('visible');
