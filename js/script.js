96cb1b52118d63bc2af69ad7829bd9a9d2b1c29a
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-07 01:23:36 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -317,7 +317,7 @@ document.addEventListener('DOMContentLoaded', () => {
       "nav.services": "الخدمات",
       "nav.projects": "المشاريع",
       "nav.contact": "اتصل بي",
-      "footer": "&copy; <span data-i18n-number>2025</span> عبد الرحمن السطاوي. جميع الحقوق محفوظة.",
+      "footer": "&copy; <span data-i18n-number>2025</span> عبد الرحمن الستاوي. جميع الحقوق محفوظة.",
     }
   };
 
