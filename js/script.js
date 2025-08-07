cb6a1798f0131baca54ccd7cbbee0612b8beeed7
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-07 01:11:26 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -340,6 +340,14 @@ document.querySelectorAll('input[data-i18n-value]').forEach(el => {
   }
 });
 
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach(el => {
+      const key = el.getAttribute('data-i18n');
+      if (translations[lang] && translations[lang][key]) {
+        el.innerHTML = translations[lang][key];
+      }
+      
+    });
 
     
 
