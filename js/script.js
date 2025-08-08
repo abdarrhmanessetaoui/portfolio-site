ba915290f28069210f9d2f8066a9d27d6a8af9a8
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2025-08-08 12:53:02 +0100

    auto-commit

--- js/script.js
+++ js/script.js
@@ -105,6 +105,9 @@ document.addEventListener('DOMContentLoaded', () => {
       "sou9naDesc": "A fresh, fully responsive Moroccan-style online shop for fruits and vegetables. The design is clean, mobile-friendly, and highlights seasonal offers. It includes a local-market vibe with intuitive navigation and placeholder admin controls.",
       "sou9naImgAlt": "Sou9na Fruits and Vegetables Website Preview",
       "inDev": "In Development..",
+      expenseTrackerTitle: "Expense Tracker",
+      expenseTrackerDesc: "A simple and intuitive expense tracker app to help you manage your income and expenses efficiently. Features real-time balance updates, transaction history, and a clean, responsive design.",
+     
       currencyConverterTitle: "Curreny-Converter-App",
       currencyConverterDesc: "A fast and efficient currency converter app that lets you convert between 150+ global currencies instantly. Features real-time exchange rates, dark mode, and a clean, responsive design.",
       currencyConverterImgAlt: "Currency Converter App Preview",
